5f068c208ae6f6b0c3e9790e488d9f63e675a8bc
Aselinux <aselinux@gmail.com>
2018-09-19 10:35:55 +0100

    common bash commands

--- command-modules/_bash.py
+++ command-modules/_bash.py
@@ -46,14 +46,41 @@ bash_rule = MappingRule(
             # terminal  
              "[keyboard] break": Key("c-c"),
              "new terminal": Key("ca-t"),
+             "copy terminal": Key("cs-c/3"),
+             "paste terminal": Key("cs-v/3"),
+             
+             # grep
+             "grep [e]|[e] grep": Text("grep -Ei ''") + Key("left:1"),
+             "grep directory": Text("grep -Eir '' ./") + Key("left:4"),
+             "pipe grep": Text("grep -Ei ''") + Key("left"),
+             "pipe grep": Text(" | grep -Ei ''") + Key("left"),
+             "pipe (awkward|och)": Text(" | awk '{print $NF}'") + Key("left:2"),
+             "pipe said": Text(" | sed 's///g'") + Key("left:4"),
+             "[bash] pipe cut": Text(" | cut -d' ' -f1"),
+             
+            # Cron
+             "cron tab edit": Text("crontab -e") + Key("enter"),
+             "cron tab list": Text("crontab -l") + Key("enter"),             
+            # directory
+             "make (directory|dir)": Text("mkdir "),
+             "change (directory|dir)": Text("cd "),
+             "(change mode)|C H mod|che mod": Text("chmod "),
+             "find file": Text("find ./ -iname '**'") + Key("left:2"),
+                     
+            # curl
+             "curl help": Text("curl -X --request GET -k --insecure -L --location -u --user $USER:$PASSWORD -A --user-agent 'AselCurl' -H --header 'Host: website.com' --header 'key1: value1' -b --cookie 'key1=value1; key2=value2' -v --verbose -t --trace-ascii /dev/stderr -D --dump-header /dev/stderr -d --data '{\"key1\": \"value1\", \"key2\": \"value2\"}' --url $URL "),
+             "curl shabang": Text("curl -v -k -X GET -L --url "),
              
             # Ubuntu
-             "pseudo": Text("sudo "),
-             "apt get install": Text("apt-get install"),
-             "apt get update": Text("apt-get update"),
-             "apt get upgrade": Text("apt-get upgrade"),
-             "apt [cash] search": Text("apt-cache search"),
-             "pseudo I": Text("sudo -i"),
+             "pseudo ": Text("sudo "),
+             "pseudo i": Text("sudo -i") + Key("enter"),
+             "pseudo apt get ": Text("sudo apt-get "),
+             "pseudo apt [cash] search": Text("sudo apt-cache search "),
+             "debian package": Text("dpkg "),
+             
+            # processes
+             "find process": Text("ps aux | grep -i "),
+             "process forest": Text("ps auxf | less"),
              
             # git
              "git reset hard": Text("#git reset --hard HEAD # deletes everything after last commit"),
@@ -62,7 +89,35 @@ bash_rule = MappingRule(
              "git [log] follow": Text("git log --follow "),
              "git log": Text("git log ") + Key("enter"),
              
-                          
+            # my commonly used commands
+             "netstat": Text("netstat -v4 -antulp | grep -iE ''") + Key("left:%(n)d"),
+             "E grep": Text("egrep -i ''") + Key("left:%(n)d"),
+             "pipe grep": Text(" | grep -iE ''") + Key("left:%(n)d"),
+             "pipe less": Text(" | less") + Key("enter:%(n)d"),
+             "pipe head": Text(" | head") + Key("enter:%(n)d"),
+             "pipe sort unique": Text(" | sort | uniq -c | sort -n -k1"),
+
+             "T C P dump headers": Text("sudo tcpdump -nn -i any -A -s 0 'tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' | egrep --line-buffered '^........(GET |HTTP\/|POST |HEAD )|^[A-Za-z0-9-]+: ' | sed -r 's/^.{8}(GET |HTTP\/|POST |HEAD )/\\n\\1/g'"),
+
+             "pipe zargz": Text(" | xargs -I") + Key("percent") + Text(" "),
+
+            # my common directories
+             "less var log": Text("less /var/log/") + Key("tab:2"),
+             "cat var log": Text("cat /var/log/") + Key("tab:2"),
+             "var log": Text("/var/log/") + Key("tab:2"),
+             "et see": Text("/etc/") + Key("tab:2"),
+             "vim et see": Text("vim /etc/") + Key("tab:2"),
+
+            # common bash scripting
+             "for i in": Text("for i in "),
+             "for loop": Text("for i in ; do echo $i; ; done") + Key("left/5:20"),
+
+            # my vim
+             "vim save": Key("escape, colon, w, enter"),
+             "vim quit": Key("escape, colon, q, enter"),
+             "double dell": Key("d, d"),
+             "double yank": Key("y, y"),
+        
             },
     )
 
